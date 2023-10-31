a5092d341536d78b25d73fabb44c4d22b97c48fc
Helheim 01 <gonzalezandy0809@gmail.com>
2023-10-31 16:44:07 -0600

    third comment

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const convert = require('xml-js')
+const express= require('express');
+const app= express();
+app.use(express.json());
+const port= process.env.port || 3000;
+app.listen(port,()=>console.log("Escuchando en puerto: "+port));
 
 app.put('/getJson',async (req,res)=>{
     let xml = Buffer.from(req.body.xml, 'base64');
